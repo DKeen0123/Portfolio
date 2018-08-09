5020759a019b2723d47333de7a5c2e3f679b1b9d
Daniel Keen <keendaniel01@gmail.com>
2018-08-09 20:45:09 +0100

    Add passport dependencies

--- server/app.js
+++ server/app.js
@@ -3,8 +3,12 @@ const graphqlHTTP = require('express-graphql');
 const schema = require('./graphql/schema/schema');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
+
+passport.use(new GoogleStrategy());
 
 mongoose.connect(`mongodb://${keys.db_user}:${keys.db_password}@ds217092.mlab.com:17092/portfolio-dev`);
 
